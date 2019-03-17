0d4d6ed0c1f3703f750ab7c7c073e592d3e2e3a9
truelogic <gpaoli@truelogic.software>
2019-03-17 13:03:48 -0300

    avance dashboard

--- modules/custom/dashboard/src/Controller/DashboardController.php
+++ modules/custom/dashboard/src/Controller/DashboardController.php
@@ -12,16 +12,23 @@ use Drupal\Core\Entity\Query\QueryFactory;
 class DashboardController extends ControllerBase{
 
   public function getPayments(){
-		$nids = \Drupal::entityQuery('node')->condition('type','payment')->execute();
-		$nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
-		//var_dump($nodes);
-		return $nodes;
+    $nids = \Drupal::entityQuery('node')->condition('type','payment')->execute();
+    $nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
+    return $nodes;
+  }
+
+  public function getUsersInPayments(){
+    $nids = \Drupal::entityQuery('node')->condition('type','payment')->execute();
+    $nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
+    return $nodes;
   }
 
   public function content(){
+    $nodes = $this->getPayments();
+    
     return [
       '#theme' => 'dashboard_admin',
-  		'#test_var' => $this->getPayments(),
+  		'#payments' => $nodes,
     ];
   }      
 }
